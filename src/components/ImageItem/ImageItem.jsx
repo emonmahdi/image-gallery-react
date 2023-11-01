079ef64816778e63f3abdb4567b6e5823c87b9f0
emonmahdi <developeremon1997@gmail.com>
2023-11-01 10:14:24 +0600

    selected image change the hover bg color [DONE]

--- src/components/ImageItem/ImageItem.jsx
+++ src/components/ImageItem/ImageItem.jsx
@@ -1,12 +1,32 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import "./ImageItem.css";
 
-const ImageItem = ({ src, isFeatured }) => {
+const ImageItem = ({ src, isFeatured, key }) => {
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  const toggleImageSelection = (imageId) => {
+    if (selectedImages.includes(imageId)) {
+      setSelectedImages(selectedImages.filter((id) => id !== imageId));
+    } else {
+      setSelectedImages([...selectedImages, imageId]);
+    }
+  };
+
   return (
     <div className={`image-item ${isFeatured ? "featured" : ""}`}>
       <img src={src} alt="Image" />
-      <div className="image-overlay">
-        <input type="checkbox" className="input-checkbox" />
+      <div
+        className={`image-overlay ${
+          selectedImages.includes(key) ? "selected" : ""
+        }`}
+      >
+        <input
+          type="checkbox"
+          checked={selectedImages.includes(key)}
+          onChange={() => toggleImageSelection(key)}
+          className="input-checkbox"
+        />
       </div>
     </div>
   );
